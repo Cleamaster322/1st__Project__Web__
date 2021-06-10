c0b8fc7e0d0e1f0f8032b55642077b4f545b3356
TCKACHIKSIS <e79532157848@gmail.com>
2021-06-10 20:55:27 +1000

    Сделал рабочей ссылку на восстановление пароля
    Также добавил css и html файлы для страницы восстановления пароля

--- flask_directory/app.py
+++ flask_directory/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def title():
     return render_template('/title_frame/title_frame.html')
+
+
+@app.route("/forgotten_password")
+def for_password():
+    return render_template('forgotten_password/forgotten_password.html')
