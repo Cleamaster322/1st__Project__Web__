badfd682bbb4d7b830f9b5a089fa1d30f3908e47
TCKACHIKSIS <e79532157848@gmail.com>
2021-06-13 22:10:09 +1000

    Добавил чуть чуть базы данных
    Начал добавление базы данных , а именно регистрацию.

--- flask_directory/app.py
+++ flask_directory/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Page_data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Page_data %r>' % self.id
 
 
 @app.route("/")
@@ -11,3 +24,8 @@ def title():
 @app.route("/forgotten_password")
 def for_password():
     return render_template('forgotten_password/forgotten_password.html')
+
+
+@app.route("/check_in")
+def for_database_test():
+    return render_template('check_in_page/check_in_page.html')
