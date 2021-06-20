3ef62f3837f960f6efe17c7413aee95818ef737e
KirillPogranichny <kirillpogranichny@gmail.com>
2021-06-21 02:41:49 +1000

    Независимые кнопки v1.1

--- flask_directory/static/my_followers/my_followers.js
+++ flask_directory/static/my_followers/my_followers.js
@@ -8,16 +8,21 @@ textInput.addEventListener("keyup", event => {
 
 function delUser(n) {
   var change = document.getElementsByClassName("delete_follower");
-  var color1 = '#ED4956';
-  var color2 = '#4ACFFF';
+  change[n].className = "delete_follower";
 
   if (change[n].innerHTML == "Заблокировать")
   {
     change[n].innerHTML = "Разблокировать";
+    change[n].style.borderColor = "#4ACFFF"
+    change[n].style.color = "#4ACFFF"
+    $('.delete_follower').addClass('spec_del_blue');
   }
   else
   {
     change[n].innerHTML = "Заблокировать";
+    change[n].style.borderColor = "#ED4956"
+    change[n].style.color = "#ED4956"
+    $('.delete_follower').addClass('spec_del_red');
   }
 }
 // Осталось реализовать изменение цвета при нажатии на кнопку и прикрутить к ней стили
