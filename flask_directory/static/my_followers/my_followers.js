89d733fdb005b1d4a98c47f8dbb095c396fcb990
KirillPogranichny <kirillpogranichny@gmail.com>
2021-06-22 03:01:43 +1000

    Update my_followers.js

--- flask_directory/static/my_followers/my_followers.js
+++ flask_directory/static/my_followers/my_followers.js
@@ -15,14 +15,12 @@ function delUser(n) {
     change[n].innerHTML = "Разблокировать";
     change[n].style.borderColor = "#4ACFFF"
     change[n].style.color = "#4ACFFF"
-    $('.delete_follower').addClass('spec_del_blue');
   }
   else
   {
     change[n].innerHTML = "Заблокировать";
     change[n].style.borderColor = "#ED4956"
     change[n].style.color = "#ED4956"
-    $('.delete_follower').addClass('spec_del_red');
   }
 }
 // Осталось реализовать изменение цвета при нажатии на кнопку и прикрутить к ней стили
